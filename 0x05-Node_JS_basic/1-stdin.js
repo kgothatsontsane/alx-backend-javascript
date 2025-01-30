03dd563c17d35ccd925273008d576352ae8726ca
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-30 11:48:50 +0000

    refactor stdin.js to use process.stdout.write for welcome message and update exit message format

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', function(data) {
     console.log(`Your name is: ${data.toString().trim()}`);
 });
 process.on('exit', function() {
-    console.log('This important software is now closing\n');
+    console.log('This important software is now closing\r');
 });
 process.stdin.on('end', function() {
     process.exit();
