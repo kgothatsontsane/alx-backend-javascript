ad1dbfa21408ee1db977759122e0812734d661cd
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-30 12:54:06 +0000

    fix 1-stdin.js to ensure welcome message is displayed in both TTY and non-TTY environments

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 if (process.stdin.isTTY) {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
+    process.stdout.write('Welcome to Holberton School, what is your name?\n');
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
