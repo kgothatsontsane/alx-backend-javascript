3ea346882603f8d68d145d6cac13ab04c01a5785
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-30 11:49:52 +0000

    refactor 1-stdin.js to change exit event handling and remove unnecessary exit logic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,6 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', function(data) {
     console.log(`Your name is: ${data.toString().trim()}`);
 });
-process.on('exit', function() {
+process.on('end', function() {
     console.log('This important software is now closing\r');
-});
-process.stdin.on('end', function() {
-    process.exit();
 });
